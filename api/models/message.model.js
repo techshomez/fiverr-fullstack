43cf1275b3a9e2394419134e970893bfca9fc3da
techshomez <danieltopeg@gmail.com>
2023-05-08 19:02:29 +0100

    project model schema

---
+++ api/models/message.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const messageSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.module("Message", messageSchema);
